672b1023392decb907df308b9087a9f76de75a08
unknown <sharipovazizbek574@gmail.com>
2025-06-17 16:52:23 +0500

    create login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  standalone:true
+})
+export class LoginComponent {
+
+}
