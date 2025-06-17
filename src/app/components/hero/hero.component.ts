2acc5607a1964a0b33bbd76c8e46e84a550734e4
unknown <sharipovazizbek574@gmail.com>
2025-06-17 11:37:25 +0500

    created modal window

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from "../../modal/modal.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [ModalComponent, CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+
+      showModal = false;
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
 
 }
