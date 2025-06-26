7385290e3dd9ce77181392907888c5cd5827c3c4
Umidbek <youremail@example.com>
2025-06-26 13:38:35 +0500

    O'zgarish qilindi stillarda

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,30 +5,33 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls:['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
-loginWithGoogle() {
-  this.authService.loginWithGoogle()
-    .then(result => {
-      const user = result.user;
-      if (user) {
-        // Foydalanuvchi ma'lumotlarini localStorage-ga saqlash
-        localStorage.setItem('user', JSON.stringify({
-          uid: user.uid,
-          displayName: user.displayName,
-          email: user.email,
-          photoURL: user.photoURL
-        }));
-        console.log('Logged in user:', user);
-        this.router.navigate(['/']);
-      }
-    })
-    .catch(error => {
-      console.error('Login failed:', error.message);
-    });
-}
-
+  loginWithGoogle() {
+    this.authService
+      .loginWithGoogle()
+      .then((result) => {
+        const user = result.user;
+        if (user) {
+          // Foydalanuvchi ma'lumotlarini localStorage-ga saqlash
+          localStorage.setItem(
+            'user',
+            JSON.stringify({
+              uid: user.uid,
+              displayName: user.displayName,
+              email: user.email,
+              photoURL: user.photoURL,
+            })
+          );
+          console.log('Logged in user:', user);
+          this.router.navigate(['/']);
+        }
+      })
+      .catch((error) => {
+        console.error('Login failed:', error.message);
+      });
+  }
 }
